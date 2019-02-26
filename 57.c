d9439c160d009c80b52b80da3491852aad9e10a7
MANOJTMK <46492877+MANOJTMK@users.noreply.github.com>
2019-02-26 20:20:17 +0530

    Create 57.c

---
+++ 57.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char a[100],c;
+    clrscr();
+    int i,l=0;
+    scanf("%s %c",a,&c);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]==c)
+        {
+    l++;
+    }}
+    printf("%d",l);
+    return 0;
+}
