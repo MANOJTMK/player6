43e1c7e97ca26b22a1dd6ae0b76e06401e27a5e9
MANOJTMK <46492877+MANOJTMK@users.noreply.github.com>
2019-02-26 20:13:27 +0530

    Create 55.c

---
+++ 55.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+    char a[10000],b[10000];
+    int n,m,i,l=0;
+    scanf("%[^\n]%[^\n]",a,b);
+    for(i=0;a[i]!='\0';i++);
+    n=i;
+    for(i=0;b[i]!='\0';i++)
+    m=i;
+    if(m==n)
+    {
+        for(i=0;i<n;i++)
+        {
+            if(a[i]==b[i]||a[i]==b[i]+32||a[i]==b[i]-32)
+            {
+               l++; 
+            }
+        }
+    }
+    if(l==m)
+    printf("yes");
+    else
+    printf("no");
+    getch();
+}
