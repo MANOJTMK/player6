bd6d8bc8dfe8bd6fbd5fc21a19c54d055ce11abf
MANOJTMK <46492877+MANOJTMK@users.noreply.github.com>
2019-02-26 20:18:58 +0530

    Create 56.c

---
+++ 56.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char a[100],c;
+    clrscr();
+    int i,l=0;
+    scanf("%s %c",a,&c);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]==c)
+        {
+    printf("%d",i+1);
+            break;
+        }}
+    return 0;
+}
